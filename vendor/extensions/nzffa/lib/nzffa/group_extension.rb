f60d7aba47fad9962d56fbfe17ad91ad9851866d
Benny Degezelle <benny@morse.be>
2025-06-26 08:46:20 +0200

    Stop app-error'ing on group edit screen when branch and action groups are gone

--- vendor/extensions/nzffa/lib/nzffa/group_extension.rb
+++ vendor/extensions/nzffa/lib/nzffa/group_extension.rb
@@ -3,17 +3,17 @@ module Nzffa::GroupExtension
   def self.included(klass)
     klass.class_eval do
 
-      named_scope :branches, :conditions => ['ancestry = ?', Radiant::Config['nzffa.branches_group_id'] ]
+      # named_scope :branches, :conditions => ['ancestry = ?', Radiant::Config['nzffa.branches_group_id'] ]
 
-      def self.branches_holder
-        find(Radiant::Config['nzffa.branches_group_id'])
-      end
+      # def self.branches_holder
+      #   find(Radiant::Config['nzffa.branches_group_id'])
+      # end
 
-      named_scope :action_groups, :conditions => ['ancestry = ?', Radiant::Config['nzffa.action_groups_group_id']]
+      # named_scope :action_groups, :conditions => ['ancestry = ?', Radiant::Config['nzffa.action_groups_group_id']]
 
-      def self.action_groups_holder
-        find(Radiant::Config['nzffa.action_groups_group_id'])
-      end
+      # def self.action_groups_holder
+      #   find(Radiant::Config['nzffa.action_groups_group_id'])
+      # end
 
       %w(past_members past_casual_members newsletter_editors councillors presidents secretarys treasurers
       fft_marketplace tg_magazine_nz tgm_australia tgm_everywhere_else full_membership fft_newsletter small_scale_fg_newsletter fg_levy_payer_newsletter).each do |gname|
@@ -24,31 +24,21 @@ module Nzffa::GroupExtension
         find(NzffaSettings.fft_marketplace_group_id)
       end
 
-      def self.tgm_groups_holder
-        find(Radiant::Config['nzffa.tgm_groups_group_id'])
-      end
+      # def self.tgm_groups_holder
+      #   find(Radiant::Config['nzffa.tgm_groups_group_id'])
+      # end
 
-      named_scope :tgm_groups, :conditions => ['ancestry = ?', "#{Group.tgm_groups_holder.ancestry}/#{Group.tgm_groups_holder.id}"]
+      # named_scope :tgm_groups, :conditions => ['ancestry = ?', "#{Group.tgm_groups_holder.ancestry}/#{Group.tgm_groups_holder.id}"]
     end
   end
 
   def has_annual_levy?
-    is_branch_group? || is_action_group? || is_fft_group? || is_tgm_group?
-  end
-
-  def is_branch_group?
-    ancestors.include? Group.branches_holder
-  end
-
-  def is_action_group?
-    ancestors.include? Group.action_groups_holder
+    # is_branch_group? || is_action_group? || is_fft_group? || is_tgm_group?
+    is_fft_group?
   end
 
   def is_fft_group?
     id == NzffaSettings.fft_marketplace_group_id
   end
 
-  def is_tgm_group?
-    Group.tgm_groups.map(&:id).include? id
-  end
 end
